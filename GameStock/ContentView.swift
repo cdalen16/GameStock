fc800930b6bc601ca5609fce8d3f9147e224a263
cdalen16 <cdalen@vt.edu>
2021-04-20 15:19:09 -0400

    Initial Commit

---
+++ GameStock/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GameStock
+//
+//  Created by CS3714 on 4/20/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
